172b232ca1c0d12a940005dabe98eaf211b42ca6
daflair <daflair@mac.com>
2012-04-10 06:02:49 -0400

    Implementing realtime region stats data source

---
+++ +Grasppe/+PrintUniformity/+Data/RegionStatsDataSource.m
@@ -0,0 +1,135 @@
+classdef RegionStatsDataSource < Grasppe.PrintUniformity.Data.UniformityDataSource
+  %SURFACEUNIFORMITYDATASOURCE Raw printing uniformity data source
+  %   Detailed explanation goes here
+  
+  properties
+    % LocalVariabilityDataSourceProperties = {
+    %   'TestProperty', 'Test Property', 'Labels', 'string', '';   ...
+    %   };
+    % TestProperty
+  end
+  
+  methods (Hidden)
+    function obj = RegionStatsDataSource(varargin)
+      obj = obj@Grasppe.PrintUniformity.Data.UniformityDataSource(varargin{:});
+    end
+    
+    function attachPlotObject(obj, plotObject)
+      obj.attachPlotObject@Grasppe.PrintUniformity.Data.UniformityDataSource(plotObject);
+      try plotObject.ParentAxes.setView([-90 90], true); end
+    end
+
+    function [X Y Z] = processSheetData(obj, sheetID, variableID)
+
+      [X Y Z]   = obj.processSheetData@Grasppe.PrintUniformity.Data.UniformityDataSource(sheetID, variableID);
+      
+      caseData      = obj.CaseData; ...
+        setData   	= obj.SetData; ...
+        sheetData   = obj.SheetData;
+      
+      targetFilter  = caseData.sampling.masks.Target~=1;
+      patchFilter   = setData.filterData.dataFilter~=1;
+      
+      Z(~patchFilter) = sheetData;
+      Z(targetFilter) = NaN;
+      Z(patchFilter)  = NaN;
+      
+      [Z M] = Grasppe.PrintUniformity.Data.RegionStatsDataSource.regionStatisticsFilter(Z);
+      
+      % Z(targetFilter) = NaN;
+      % Z(patchFilter)  = NaN;
+%       
+%       dataFilter  = ~isnan(Z);
+%       
+%       F = TriScatteredInterp(X(dataFilter), Y(dataFilter), Z(dataFilter), 'natural');
+%       
+%       Z = F(X, Y);
+%       Z(targetFilter) = NaN;
+      
+      %Z(patchFilter~=1)   = NaN;      
+      
+    end
+    
+    function optimizeSetLimits(obj)
+      zLim    = 'auto';
+      
+      obj.ZLim  = zLim;
+      obj.CLim  = zLim;
+    end
+    
+    function setPlotData(obj, XData, YData, ZData)
+      try debugStamp(obj.ID); catch, debugStamp(); end;
+      obj.XData = XData;
+      obj.YData = YData;
+      %obj.ZData = ZData;
+      obj.CData = ZData;
+      
+      %ZData(~isnan(ZData)) = nanmean(ZData(:));
+      
+      obj.ZData = ZData;
+      
+      obj.updatePlots();
+    end
+    
+  end
+  
+  methods (Static, Hidden)
+    function [newData metrics]  = regionStatisticsFilter(zData)
+      %newData = zData;
+      
+      rows    = size(zData, 1);
+      columns = size(zData, 2);
+      
+      if rows > columns
+        across = 3;
+        around = round(((rows/columns*3)-1)/2)*2+1;
+      else % rows <= columns
+        around = 3;
+        across = round(((columns/rows*3)-1)/2)*2+1;
+      end
+      
+      rowSteps          = reshape(([(rows)/around] * ceil(0:0.5:around-0.5)),2,[]);
+      rowSteps          = round(rowSteps);
+      rowSteps(1,:)     = rowSteps(1,:) + 1;
+      
+      columnSteps       = reshape(([(columns)/across] * ceil(0:0.5:across-0.5)),2,[]);
+      columnSteps       = round(columnSteps);
+      columnSteps(1,:)  = columnSteps(1,:) + 1;
+      
+      % try
+      newData = zeros(size(zData));
+      
+      values  = zeros(around, across);
+      
+      for r = 1:around
+        row = round(rowSteps(1,r):rowSteps(2,r));
+        for c = 1:across
+          column  = round(columnSteps(1,c):columnSteps(2,c));
+          cellData              = zData(row, column);
+          values(r,c)           = nanmean(cellData(:)); % ([(r-1)*across]+c)/(around*across)
+          newData(row, column)  = values(r,c); % 1/(around*across)*(r*(c-1)+1);
+        end
+      end
+      
+      metrics.rowBands    = around;
+      metrics.columnBands = across;
+      metrics.rowSteps    = rowSteps;
+      metrics.columnSteps = columnSteps;
+      metrics.values      = values;
+      % end
+    end
+    
+    function OPTIONS  = DefaultOptions()      
+      Grasppe.Utilities.DeclareOptions;
+    end
+  end
+  
+  methods (Static)
+    function obj = Create(varargin)
+      obj = Grasppe.PrintUniformity.Data.LocalVariabilityDataSource(varargin{:});
+    end
+  end
+  
+  
+end
+
