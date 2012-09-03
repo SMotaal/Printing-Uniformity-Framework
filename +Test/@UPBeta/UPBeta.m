62d016190c8b0c24f01c6eba79c817d729015a98
daflair <daflair@mac.com>
2012-09-03 11:27:14 -0400

    Added @UPBeta.TestDataReader

---
+++ +Test/@UPBeta/UPBeta.m
@@ -0,0 +1,12 @@
+classdef UPBeta
+  %UPDATAREADER2 Summary of this class goes here
+  %   Detailed explanation goes here
+  
+  properties
+  end
+  
+  methods (Static)
+    TestDataReader2();
+  end  
+end
+
