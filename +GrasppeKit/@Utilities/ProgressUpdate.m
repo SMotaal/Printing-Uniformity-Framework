837d399b7ddd5d4e8dab660f49abf96c85c88984
daflair <daflair@mac.com>
2013-05-26 09:21:43 -0400

    Added StatusBar alternative to WaitBar method for Utilities.ProgressUpdate

--- +GrasppeKit/@Utilities/ProgressUpdate.m
+++ +GrasppeKit/@Utilities/ProgressUpdate.m
@@ -1,7 +1,52 @@
 function ProgressUpdate(progress, varargin)
-  %SETSTATU Summary of this function goes here
+  %ProgressUpdate Summary of this function goes here
   %   Detailed explanation goes here
   
+  method                    = 'statusbar';
+  
+  switch lower(method)
+    case 'waitbar'
+      if ~exist('progress', 'var')
+        setWaitbar();
+      else
+        setWaitbar(progress, varargin{:});
+      end
+    case 'statusbar'
+      if ~exist('progress', 'var')
+        setStatusBar();
+      else
+        setStatusBar(progress, varargin{:});
+      end
+  end
+end
+
+function setStatusBar(progress, varargin)
+  if ~exist('progress', 'var') || isempty(progress) || ~isnumeric(progress)
+    statusbar(0, '');
+  else
+    progressPrefix        = 'Processing';
+    progressString        = '';
+    try progressPrefix    = varargin{1}; end
+    if isequal(progress, 1)
+      try progressString  = ['<html><b>' strtrim(progressPrefix) ': </b>Done!</html>']; end
+      try GrasppeKit.Utilities.DelayedCall(@(s, e) statusbar(0, ''), 10, 'start'); end
+    else
+      try progressString  = ...
+          ['<html><b>' strtrim(progressPrefix) ': </b>' ...
+          num2str(progress*100, '%1.0f%%') ' complete'...
+          '</html>']; end
+    end
+    try statusbar(0, regexprep(progressString, '%', '%%')); end
+  end
+  
+  try 
+    GrasppeKit.Utilities.DelayedCall(@(s, e) drawnow('expose', 'update'), 1, 'start'); 
+  catch err
+    drawnow('expose', 'update');
+  end
+end
+
+function setWaitbar(progress, varargin)
   persistent hProgress mProgress;
   
   if ~exist('progress', 'var') || isempty(progress)
@@ -33,7 +78,7 @@ function ProgressUpdate(progress, varargin)
   
   
   jProgress = get(handle(hProgress),'JavaFrame');
-
+  
   try
     jProgress.fFigureClient.getWindow().setAlwaysOnTop(true);
   catch err
@@ -43,5 +88,5 @@ function ProgressUpdate(progress, varargin)
   
   titleHandle               = get(findobj(hProgress,'Type','axes'),'Title');
   set(titleHandle,'FontSize', 8, 'FontWeight', 'bold');
+  
 end
-
